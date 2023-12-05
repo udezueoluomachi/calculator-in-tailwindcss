0234bff31c4bf6e4f13473ecf0f2fdc02710a844
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-12-05 19:09:52 +0100

    migration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-
 function App() {
   return (
-    <div className="App">
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-center">Tailwind CSS Example</h1>
+      <p className="text-lg text-gray-500">This is a paragraph with some text.</p>
+      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">Click Me</button>
     </div>
   );
 }
