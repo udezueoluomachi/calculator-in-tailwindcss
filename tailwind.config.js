def21023e365c488f78aafb16a6c0bf1dfac0d69
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-12-05 14:08:14 +0100

    learning tailwindcss configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors : {
+      "black" : "#000000",
+      "off-white" : "#E0E0E0",
+      "grey" : "#2F2F2F",
+      "orange" : "#F19305"
+    },
     extend: {},
   },
   variants: {
